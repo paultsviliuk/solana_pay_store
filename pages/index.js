b15cf9485368d37c91de76570757a25559e29d02
Paul <paultsvilyuk@gmail.com>
2024-06-23 11:44:13 +0200

    products added.

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import { PublicKey } from '@solana/web3.js';
+import React, { useEffect, useState } from 'react';
+import Product from "../components/Product";
+
 import { useWallet } from '@solana/wallet-adapter-react';
-import dynamic from "next/dynamic";
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 // Constants
 const TWITTER_HANDLE = "thepaultsviliuk";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-// Dynamic import `WalletMultiButton` to prevent hydration error
-const WalletMultiButtonDynamic = dynamic(
-    async () =>
-      (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
-    { ssr: false }
-  );
-  
-  // This will fetch the users' public key (wallet address) from any wallet we support
   const { publicKey } = useWallet();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
 
   const renderNotConnectedContainer = () => (
-    <div>
-      <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
-
-      <div className="button-container">
-        <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
-      </div>    
+    <div className="button-container">
+      <WalletMultiButton className="cta-button connect-wallet-button" />
+    </div>
+  );
+  
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
     </div>
   );
 
@@ -37,9 +46,7 @@ const WalletMultiButtonDynamic = dynamic(
         </header>
 
         <main>
-          {/* We only render the connect button if public key doesn't exist */}
-          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
-
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
